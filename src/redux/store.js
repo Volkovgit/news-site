ec9dc6b1c864c0b9c5880645eb18f06a042a5ac7
volkovgit <volkovgit@gmail.com>
2022-06-26 22:28:14 +0300

    ...

--- src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import {createStore,applyMiddleware} from 'redux';
-import {newsReducer} from './rootReducer'
-import thunk from 'redux-thunk';
-
-const store = createStore(newsReducer,applyMiddleware(thunk));
-
-export default store;
